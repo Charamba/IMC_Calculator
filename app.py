f74bb6a53466ac26d76ea63dd3bdcf2b705b0353
Luiz Gustavo da Rocha Charamba <charamba.lgr@gmail.com>
2025-03-13 18:03:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,12 +32,45 @@ def imc_status(imc):
         caution_status = 2
     return imc_status, caution_status
 
+def imc_result_text(imc_status):
+    result_text = ""
+
+    if imc_status == "Abaixo do normal":
+        result_text = "Procure um médico. Algumas pessoas têm um baixo peso por características do seu organismo e tudo bem. Outras podem estar enfrentando problemas, como a desnutrição. É preciso saber qual é o caso."
+    elif imc_status == "Normal":
+        result_text = "Que bom que você está com o peso normal! E o melhor jeito de continuar assim é mantendo um estilo de vida ativo e uma alimentação equilibrada."
+    elif imc_status == "Sobrepeso":
+        result_text = "Ele é, na verdade, uma pré-obesidade e muitas pessoas nessa faixa já apresentam doenças associadas, como diabetes e hipertensão. Importante rever hábitos e buscar ajuda antes de, por uma série de fatores, entrar na faixa da obesidade pra valer."
+    elif imc_status == "Obesidade grau 1":
+        result_text = "Sinal de alerta! Chegou na hora de se cuidar, mesmo que seus exames sejam normais. Vamos dar início a mudanças hoje! Cuide de sua alimentação. Você precisa iniciar um acompanhamento com nutricionista e/ou endocrinologista."
+    elif imc_status == "Obesidade grau 2":
+        result_text = "Mesmo que seus exames aparentem estar normais, é hora de se cuidar, iniciando mudanças no estilo de vida com o acompanhamento próximo de profissionais de saúde."
+    elif imc_status == "Obesidade grau 3":
+        result_text =  "Aqui o sinal é vermelho, com forte probabilidade de já existirem doenças muito graves associadas. O tratamento deve ser ainda mais urgente."
+    
+    return result_text
+
+
+
 # Função para calcular a TMB
 def calcular_tmb(sexo, peso, altura, idade):
     if sexo == "Homem":
         return 88.36 + (13.4 * peso) + (4.8 * altura) - (5.7 * idade)
     else:
         return 447.6 + (9.2 * peso) + (3.1 * altura) - (4.3 * idade)
+
+# 
+def calcular_get(tmb, atividade):
+    if atividade == "Sedentário":
+        return tmb*1.2
+    elif atividade == "Levemente ativo":
+        return tmb*1.375
+    elif atividade == "Moderadamente ativo":
+        return tmb*1.55
+    elif atividade == "Muito ativo":
+        return tmb*1.725
+    elif atividade == "Extremamente ativo":
+        return tmb*1.9
 
 # Função para calcular o percentual de gordura corporal
 def calcular_gordura(sexo, altura, pescoco, cintura, quadril=None):
@@ -70,10 +103,11 @@ def calcular_macros(calorias, objetivo):
     return carbs_g, prot_g, gord_g
 
 # Criando abas no Streamlit
-aba = st.sidebar.radio("Escolha uma calculadora", ["IMC", "TMB", "Gordura Corporal", "Peso Ideal", "Macronutrientes"])
+aba = st.sidebar.radio("Escolha uma calculadora", ["IMC", "Gasto Energético", "Gordura Corporal", "Peso Ideal", "Macronutrientes"])
 
 if aba == "IMC":
-    st.header("📏 Índice de Massa Corporal (IMC)")
+    st.header("📏 Calculadora de Índice de Massa Corporal (IMC)")
+    st.write("O **Índice de Massa Corporal (IMC)**, é um parâmetro utilizado para avaliar se seu peso está dentro do valor ideal para a sua altura. O IMC é calculado dividindo seu peso pelo o quadrado da sua altura.")
     peso = st.number_input("Peso (kg)", min_value=10.0, max_value=300.0, step=0.1)
     altura = st.number_input("Altura (m)", min_value=0.5, max_value=2.5, step=0.01)
     
@@ -85,28 +119,48 @@ if aba == "IMC":
         imc_status, caution_status = imc_status(imc)
 
         if caution_status == 3:
-            st.error(imc_status)
+            st.error("**" + imc_status + "**", icon="🚨")
         elif caution_status == 2:
-            st.warning(imc_status)
+            st.warning("**" + imc_status + "**", icon="⚠️")
         elif caution_status == 1:
-            st.success(imc_status)
+            st.success("**" + imc_status + "**", icon="✅")
 
-elif aba == "TMB":
-    st.header("🔥 Taxa Metabólica Basal (TMB)")
+        st.write(imc_result_text(imc_status))
+
+elif aba == "Gasto Energético":
+    st.header("🔋Calculadora de Gasto Energético")
+    st.subheader("🔥 Taxa Metabólica Basal (TMB)")
+    st.write("A **Taxa Metabólica Basal (TMB)** é a quantidade mínima de energia que o corpo precisa para manter as funções vitais em repouso, como respiração, circulação e temperatura corporal. Ela representa cerca de 60% a 70% do gasto energético total diário e varia de acordo com sexo, idade, peso e altura.")
     sexo = st.selectbox("Sexo", ["Homem", "Mulher"])
     peso = st.number_input("Peso (kg)", min_value=10.0, max_value=300.0, step=0.1)
     altura = st.number_input("Altura (m)", min_value=0.5, max_value=2.5, step=0.01)
     idade = st.number_input("Idade", min_value=10, max_value=120, step=1)
 
     altura_cm = altura*100
-    
-    if st.button("Calcular TMB"):
+
+    st.subheader("⚡ Multiplique pelo Nível de Atividade Física")
+    st.write("Depois de calcular a TMB, multiplicamos esse valor por um fator de atividade para obter o **Gasto Energético Total (GET)**, que representa o total de calorias diárias considerando seu estilo de vida:")
+
+    st.write("🛋️ **Sedentário** (pouco ou nenhum exercício): TMB × 1.2")
+    st.write("🚶 **Levemente ativo** (exercício leve 1-3 dias/semana): TMB × 1.375")
+    st.write("🏃 **Moderadamente ativo** (exercício moderado 3-5 dias/semana): TMB × 1.55")
+    st.write("🏋️ **Muito ativo** (exercício intenso 6-7 dias/semana): TMB × 1.725")
+    st.write("🏆 **Extremamente ativo** (atletas ou trabalho físico intenso): TMB × 1.9")
+
+    atividade = st.selectbox("Nível de Atividade Física", ["Sedentário", "Levemente ativo", "Moderadamente ativo", "Muito ativo", "Extremamente ativo"])
+
+    if st.button("Calcular Gasto Energético"):
         tmb = calcular_tmb(sexo, peso, altura_cm, idade)
-        st.write("Sua TMB é:")
+        st.write("Sua Taxa Metabólica Basal é:")
         st.subheader(f"{tmb:.2f} kcal/dia")
 
+        get = calcular_get(tmb, atividade)
+        st.write("Seu Gasto Energético Total é:")
+        st.subheader(f"{get:.2f} kcal/dia")
+
+
 elif aba == "Gordura Corporal":
-    st.header("⚖️ Gordura Corporal")
+    st.header("⚖️ Calculadora de Gordura Corporal")
     sexo = st.selectbox("Sexo", ["Homem", "Mulher"])
     altura = st.number_input("Altura (m)", min_value=0.5, max_value=2.5, step=0.01)
     pescoco = st.number_input("Circunferência do Pescoço (cm)", min_value=20.0, max_value=70.0, step=0.1)
@@ -125,7 +179,7 @@ elif aba == "Gordura Corporal":
         st.subheader(f"{gordura:.2f} %")
 
 elif aba == "Peso Ideal":
-    st.header("🏋️ Peso Ideal")
+    st.header("🏋️ Calculadora de Peso Ideal")
     sexo = st.selectbox("Sexo", ["Homem", "Mulher"])
     altura = st.number_input("Altura (m)", min_value=0.5, max_value=2.5, step=0.01)
 
